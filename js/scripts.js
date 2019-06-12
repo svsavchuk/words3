4fb9c3096783849610da707d047d5ca77ed8cb37
Student <example@example.com>
2019-06-12 16:43:46 -0700

    Initial commit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,87 +1,25 @@
-$(document).ready(function () {
-  $("form").submit(function(event){
+$(document).ready(function() {
+  $("form").submit(function(event) {
     event.preventDefault();
-    var input = $("input#wordInput").val();
-    var pigInput = translateSentence(input);
-    $(".outputtext").text(pigInput);
+
+  var englishVowels = ["a","e","i","o","u"]
+  var totalWovels = 0;
+
+  var userInput = ($("input#wordinput").val()).split("");
+  var lowercaseUserInput = userInput.toLowerCase();
+  //
+  // lowercaseUserInput.forEach(function(character) {
+
+
   });
-});
 
-function firstIsVowel(word){
-  return word + "ay";
-}
-
-function firstConsonants(word){
-  word += ",";
-    while(isConsonant(word.charAt(0)) && word.charAt(0) !== ","){
-      word = word.slice(1) + word.charAt(0);
-    }
-    if (word.charAt(word.length -1).toLowerCase() === 'q'){
-      if (word.charAt(0).toLowerCase() === 'u'){
-        word = word.slice(1) + word.charAt(0);
-      }
-    }
-  return removeSpecial(word) + "ay";
-}
-
-function firstLetterVowel(word){
-  return isVowel(word.charAt(0));
-}
-
-function isVowel(character){
-  var vowels = ["a", "e", "i", "o", "u"];
-  return vowels.includes(character.toLowerCase());
-}
-
-function translateOneWord(sentence){
-  if (isVowel(sentence.charAt(0))){
-    sentence = firstIsVowel(sentence);
-  } else if (isConsonant(sentence.charAt(0))){
-    sentence = firstConsonants(sentence);
-  }
-  return sentence;
-}
+// split into individual characters
+//evaluate if each character is a vowels
+// if so, add +1 to var totalVowels
 
 
-function isConsonant(character){
-  var consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
-  return consonants.includes(character.toLowerCase());
-}
-
-function translateSentence(sentence){
-  var splitSentence = sentence.split(" ");
-  var translateResults = [];
-  splitSentence.forEach(
-    function (word){
-      translateResults.push(translateOneWord(word));
-    }
-  )
-  var finalString = translateResults.join(" ");
-  return finalString;
-}
-
-//working if remove not function,
-
-
-
-function allConsonants(word){
-  for(var i = 0; i < word.length; i++){
-    if (!isConsonant(word.charAt(i))){
-      return false;
-    }
-  }
-  return true;
-}
-
-function removeSpecial(word){
-  var commaPos;
-  for (var i = 0; i < word.length; i++){
-    if (word.charAt(i) === ","){
-      commaPos = i;
-    }
-  }
-  if (commaPos !== undefined){
-    word = word.slice(0, commaPos) + word.slice(commaPos + 1, word.length);
-  }
-  return word;
-}
+    // alert(userInput)
+    // alert("Hi")
+    $("#result").append("<br>"+userInput+"</br>");
+  });
+// });
